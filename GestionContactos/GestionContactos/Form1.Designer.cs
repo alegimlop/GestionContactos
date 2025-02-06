8273df7a76b7a0483c3518b5437d4a215b2beab4
ALEJANDRO GIMÉNEZ LÓPEZ <alegimlop@alu.edu.gva.es>
2025-02-06 16:33:47 +0100

    Primer botón Alejandro

---
+++ GestionContactos/GestionContactos/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GestionContactos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAñadirContacto = new Button();
+            btnModificarContacto = new Button();
+            btnEliminarContacto = new Button();
+            btnMostarContacto = new Button();
+            SuspendLayout();
+            // 
+            // btnAñadirContacto
+            // 
+            btnAñadirContacto.Location = new Point(50, 45);
+            btnAñadirContacto.Name = "btnAñadirContacto";
+            btnAñadirContacto.Size = new Size(96, 55);
+            btnAñadirContacto.TabIndex = 0;
+            btnAñadirContacto.Text = "Añadir contacto";
+            btnAñadirContacto.UseVisualStyleBackColor = true;
+            btnAñadirContacto.Click += btnAñadirContacto_Click;
+            // 
+            // btnModificarContacto
+            // 
+            btnModificarContacto.Location = new Point(50, 129);
+            btnModificarContacto.Name = "btnModificarContacto";
+            btnModificarContacto.Size = new Size(96, 55);
+            btnModificarContacto.TabIndex = 1;
+            btnModificarContacto.Text = "Modificar contacto";
+            btnModificarContacto.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarContacto
+            // 
+            btnEliminarContacto.Location = new Point(167, 129);
+            btnEliminarContacto.Name = "btnEliminarContacto";
+            btnEliminarContacto.Size = new Size(96, 55);
+            btnEliminarContacto.TabIndex = 2;
+            btnEliminarContacto.Text = "Eliminar contacto";
+            btnEliminarContacto.UseVisualStyleBackColor = true;
+            // 
+            // btnMostarContacto
+            // 
+            btnMostarContacto.Location = new Point(167, 45);
+            btnMostarContacto.Name = "btnMostarContacto";
+            btnMostarContacto.Size = new Size(96, 55);
+            btnMostarContacto.TabIndex = 3;
+            btnMostarContacto.Text = "Mostar contacto";
+            btnMostarContacto.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(328, 234);
+            Controls.Add(btnMostarContacto);
+            Controls.Add(btnEliminarContacto);
+            Controls.Add(btnModificarContacto);
+            Controls.Add(btnAñadirContacto);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnAñadirContacto;
+        private Button btnModificarContacto;
+        private Button btnEliminarContacto;
+        private Button btnMostarContacto;
+    }
+}
---
+++ GestionContactos/GestionContactos/Form1.cs
@@ -0,0 +1,85 @@
+using Microsoft.VisualBasic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace GestionContactos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const int TAM = 10;
+        string[] nombre = new string[TAM];
+        string[] telef = new string[TAM];
+
+        bool LeerContacto(string[] nom, string[] tlf)
+        {
+            string nombre, telefono;
+            nombre = telefono = string.Empty;
+            bool repe;
+            bool result = true;
+
+            nombre = Interaction.InputBox("Introduce el nombre del contacto que desea añadir:");
+            repe = ComproNom(nombre, nom);
+
+            if (!repe)
+            {
+                telefono = Interaction.InputBox("Introduce el número de teléfono del contacto que desea añadir:");
+                result = AñadirContacto(nombre, telefono, nom, tlf);
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+        bool AñadirContacto(string nombre, string telefono, string[] nom, string[] tlf)
+        {
+            int i = 0;
+            bool vacia = false;
+            while (i < nom.Length && !vacia)
+            {
+                if (nom[i] == null)
+                {
+                    nom[i] = nombre;
+                    tlf[i] = telefono;
+                    vacia = true;
+                }
+                i++;
+            }
+            return vacia;
+        }
+        bool ComproNom(string nom, string[] nombre)
+        {
+            bool repet = false;
+
+            int i = 0;
+            while (!repet && i < nombre.Length)
+            {
+                if (nom == nombre[i])
+                {
+                    repet = true;
+                }
+                i++;
+            }
+            return repet;
+        }
+        private void btnAñadirContacto_Click(object sender, EventArgs e)
+        {
+            bool result;
+
+            result = LeerContacto(nombre, telef);
+
+            if (result)
+            {
+                MessageBox.Show("Se ha añadido el contacto.");
+            }
+            else
+            {
+                MessageBox.Show("No se ha podido añadir el contacto porque el contacto ya existe o no hay más huecos en los contactos.");
+            }
+        }
+    }
+}
